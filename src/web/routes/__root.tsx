1d42a449ea71e0d2e0465d86b29a124a41fe98d8
eci4ever <eci4ever@gmail.com>
2026-06-23 16:12:54 +0800

    Add sidebar-07 block with dashboard layout, TooltipProvider in root

--- src/web/routes/__root.tsx
+++ src/web/routes/__root.tsx
@@ -1,8 +1,13 @@
 import { createRootRoute } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 function RootLayout() {
-	return <Outlet />;
+	return (
+		<TooltipProvider>
+			<Outlet />
+		</TooltipProvider>
+	);
 }
 
 export const Route = createRootRoute({
